d966743a6549a56b23a996204a82b58e6909e42f
sowmiya <sowmikutty000@gmail.com>
2022-05-24 11:38:21 +0530

    new code

---
+++ src/org/test/Emp.java
@@ -0,0 +1,21 @@
+package org.test;
+
+public class Emp {
+	public void empId() {
+		System.out.println("Employee Id is 22");
+			
+	}
+	public void empName() {
+		System.out.println("Employee name is baby");
+		}
+	public static void main(String[] args) {
+		Emp v = new Emp();
+		v.empId();
+		v.empName();
+		
+		
+	}
+	
+	
+
+}
